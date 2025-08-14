ef53f2c6bc0c38c7ff82f9b6db819ccf5363f0aa
Victor-V21 <vegav2605@gmail.com>
2025-08-14 12:10:04 -0600

    fix login

--- mi-campus-app/src/router/AppRouter.tsx
+++ mi-campus-app/src/router/AppRouter.tsx
@@ -1,18 +1,17 @@
 import { Route, Routes, Navigate } from "react-router" //Navigate lo colocamos para que redirija a la ruta principal si el usuario ya está autenticado
 import { Navbar } from "../presentation/components/layout/Navbar"
 import { HomePage } from "../presentation/pages/home/HomePage"
-//import { PrivateRoute } from "../presentation/components/shared/PrivateRoute"
+import { PrivateRoute } from "../presentation/components/shared/PrivateRoute"
 import { useAuthStore } from "../presentation/stores/authStore"
 import { ProfilePage } from "../presentation/pages/profile/ProfileHome"
 import { LoginPage } from "../presentation/pages/security/auth/LoginPage"
 import { EventoPage } from "../presentation/pages/eventos/EventosPage"
-//import { MapPage } from "../presentation/pages/Map/MapPage"
+import { MapPage } from "../presentation/pages/Map/MapPage"
 import { RegisterPage } from "../presentation/pages/security/auth/RegisterPage";
 import { IndexPage } from "../presentation/pages/home/IndexPage"
-import { PublicationPage } from "../presentation/pages/eventos/PublicationPage"
-// import { GeminiChatbot } from "../presentation/pages/Chatbot/ChatBotPage"
-// import { VerifyEmailPage } from "../presentation/pages/security/auth/VerifyEmailPage"
-// import AdminCampusesPage from "../presentation/pages/AdminPanel/AdminCampusesPage"
+import { GeminiChatbot } from "../presentation/pages/Chatbot/ChatBotPage"
+import { VerifyEmailPage } from "../presentation/pages/security/auth/VerifyEmailPage"
+import { AdminCampusesPage } from "../presentation/pages/AdminPanel/AdminCampusesPage"
 
 //Private Route es un componente que protege las rutas que requieren autenticación tiene fallos por eso está comentado
 export const AppRouter = () => {
@@ -24,36 +23,21 @@ export const AppRouter = () => {
                 <Route path="/" element={<HomePage />} />
 
                 {/* Ruta protegida que requiere autenticación */}
-<<<<<<< HEAD
-                 {/*<Route element={<PrivateRoute/>}> */} 
-                    <Route element={<Navbar />}>
-                    <Route path="/index" element={<IndexPage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                    <Route path="/eventos" element={<EventoPage />} />
-                    <Route path="/mapa" element={<MapPage/>} />
-                </Route>
-                {/*Auth de login y registro */}
-              {/*</Route> */}  
-                <Route path="/auth">
-                    <Route path="login" element={authenticated ? <Navigate to="/index" replace/> : <LoginPage />} />
-                    <Route path="register"element={authenticated ? <Navigate to="/index" replace /> : <RegisterPage />}/>
-=======
-                {/* <Route element={<PrivateRoute />}> */}
+                <Route element={<PrivateRoute />}>
                     <Route element={<Navbar />}>
                         <Route path="/index" element={<IndexPage />} />
-                        {/* <Route path="/chatbot" element={<GeminiChatbot />} /> */}
+                        <Route path="/chatbot" element={<GeminiChatbot />} />
                         <Route path="/profile" element={<ProfilePage />} />
                         <Route path="/eventos" element={<EventoPage />} />
-                        <Route path="/publication" element={<PublicationPage />} />
-                        {/* <Route path="*" element={<AdminCampusesPage />}></Route> */}
+                        <Route path="/publication" element={<MapPage />} />
+                        <Route path="*" element={<AdminCampusesPage />}></Route>
                     </Route>
                     {/*Auth de login y registro */}
-                {/* </Route> */}
+                </Route>
                 <Route path="/auth">
                     <Route path="login" element={authenticated ? <Navigate to="/index" replace /> : <LoginPage />} />
                     <Route path="register" element={authenticated ? <Navigate to="/index" replace /> : <RegisterPage />} />
-                    {/* <Route path="/auth/verify-email" element={<VerifyEmailPage />} /> */}
->>>>>>> homafrontend
+                    <Route path="/auth/verify-email" element={<VerifyEmailPage />} />
                 </Route>
             </Routes>
         </div>
